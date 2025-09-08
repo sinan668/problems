31134a76d8a96de55f2efa9f2668817c50728291
muhammed sinan m <Muhammedsinanvavoor@gmail.com>
2025-09-08 14:12:09 +0530

    solved a problem to find the sum of all elements in an array

---
+++ object.js
@@ -0,0 +1,25 @@
+// sum of arry
+
+
+let arr = [2,3,4,6,5,6,7];
+
+for (i = 0 ; i < arr.length ; i++){
+    console.log(arr[i]);
+}
+
+let arry = [ 0,1, 2, 3, 4, 5];
+
+console.log(arry.length);  //6
+
+console.log(arry[0]); //0
+console.log(arry[1]); //1
+console.log(arry[2]); //2
+console.log(arry[3]); //3
+console.log(arry[4]); //4
+console.log(arry[5]); //5
+
+
+
+
+
+
