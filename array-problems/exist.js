e7345d1d3a4bca6cdbb96b35d7f28c7481f6838a
muhammed sinan <muhammedsinanvavoor@gamil.com>
2025-10-27 16:16:36 +0530

    how to find a Number were exist in array

---
+++ array-problems/exist.js
@@ -0,0 +1,13 @@
+// how to find a Number were exist in array
+
+
+let array =[2,3,4,6,3]
+
+let exist = 3;
+
+for(i = 0; i <array.length;i++){
+    if (array[i] === exist){
+        console.log(true);
+        
+    }
+}
