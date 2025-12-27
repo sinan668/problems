859cfb1294568e5ed961222efe9095e9681e3f68
muhammed sinan <muhammedsinanvavoor@gmail.com>
2025-12-27 11:51:43 +0530

    arithematical operaters 1-Addition,2-Subtraction,3- multipplication ,4-division ,5-Modulus,6-Exponentiation,7- Increment,8-dicrement

---
+++ operaters/arithematical.js
@@ -0,0 +1,78 @@
+
+
+// 1-Addition
+
+
+let a = 3
+
+let b = 5
+
+
+console.log(a+b);
+
+
+// 2-Subtraction
+
+let c = 3
+
+let d = 1
+
+
+console.log(c-d);
+
+
+// 3- multipplication 
+
+let e = 4
+
+let f = 5 
+
+console.log(e*f);
+
+
+// 4-division 
+
+
+let g = 20 
+
+let h = 2
+
+console.log(g/h);
+
+
+// 5-Modulus
+
+let i = 30
+
+let j = 4
+
+console.log(i%j);
+
+
+// 6-Exponentiation
+
+let k = 2
+
+let l = 3
+
+console.log(k**l);
+
+
+// 7- Increment
+
+let m = 5  
+
+m++
+
+console.log(m);
+
+
+
+
+// 8-dicrement 
+
+let n = 5
+
+n--
+
+console.log(n);
