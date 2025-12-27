8ded426bc30f88cf3d24dd2eb3bce0c749579f13
muhammed sinan <muhammedsinanvavoor@gmail.com>
2025-12-27 15:30:06 +0530

    creat switch case with contesion

---
+++ Control-Flow/switch-cade.js
@@ -0,0 +1,30 @@
+let day = 5
+
+switch(day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    default:
+        console.log("Other day");
+}
+
+
+
+let grade = "B";
+
+switch (grade) {
+    case "A":
+        console.log("Excellent");
+        break;
+    case "B":
+        console.log("Good");
+        break;
+    default:
+        console.log("Fail");
+}
