71659ec9842f5d46e6c0a8951ad7a873e02d98bf
muhammed sinan <muhammedsinanvavoor@gamil.com>
2025-12-27 10:34:27 +0530

    there is two type of data one of them primitive with examples

---
+++ data-type/primitive.js
@@ -0,0 +1,91 @@
+// primitive
+
+
+/*
+
+ 1- string
+ 2- number
+ 3- boolean
+ 4- bigint
+ 5- undefine
+ 6- null
+ 7- symbol
+
+*/
+
+
+// 1- String
+
+let string = 'string'
+
+console.log(typeof(string));
+
+
+
+
+
+// 2- Number
+
+let number = 123
+
+console.log(typeof(number));
+
+
+
+
+
+
+
+// 3- Boolean
+
+let Boolean = true
+let boolean = false
+
+console.log(typeof(boolean));
+
+
+
+
+
+// 4- BigInt 
+
+let bigint = 9868646353877563535n
+
+console.log(typeof(bigint));
+
+
+
+
+
+// 5-undefined
+
+let undefine ;
+
+console.log(typeof(undefine));
+
+
+
+
+// 6- Null
+
+let Null = null
+
+console.log(typeof(Null));
+
+
+
+
+
+
+// 7-Symbol 
+
+let user = {
+
+    name:'sinan'
+}
+
+let userId = 'Id'
+
+user[userId] = 123
+
+console.log(user[userId]);
