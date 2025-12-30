49f31c362966f80bef4142b1ee301a4c68fed957
muhammed sinan <muhammedsinanvavoor@gmail.com>
2025-12-30 09:53:58 +0530

    creat a function and how to give parameeter and argument and hoe to reuse it

---
+++ functions/function.js
@@ -0,0 +1,30 @@
+// function is a block of reuseble code to perform a task 
+
+
+
+function abc(){
+
+    console.log('this is a function');
+
+}
+
+abc()
+
+
+// function with argument and parameeter
+
+
+function def(a,b){
+    return(a+b);
+}
+
+let num1 = 20
+let num2 = 30
+
+let ans = def(num1,num2)
+
+console.log(ans);
+
+
+// scope
+
