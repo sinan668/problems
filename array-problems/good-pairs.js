147e2a919d675fadc0f89287f6a14482671f5743
muhammed sinan <muhammedsinanvavoor@gmail.com>
2026-01-06 17:36:19 +0530

    how to find a good paire ,find same number and find howmany good paire in there

---
+++ array-problems/good-pairs.js
@@ -0,0 +1,13 @@
+let nums =[1,2,3,1,1,3]
+
+let a = 0;
+for(let i = 0;i<nums.length;i++){
+    for(let j = i+1; j<nums.length;j++){
+        
+        if(nums[i] == nums[j]){
+            a++
+        }
+    }
+}
+
+console.log(a);
